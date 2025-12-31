2f48ee6275dd9271e7e3c6446a7a32848caede49
G-BAT <batman02970@gmail.com>
2025-12-31 16:56:43 +0530

    Fixed wildcard route error

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-    app.get('*', (req, res) => {
+    app.get((req, res) => {
         res.sendFile(path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html'));
     });
 } else {
