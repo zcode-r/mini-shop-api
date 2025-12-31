fe418df0ebaece9c22d7ca9af046073035f04a1f
G-BAT <batman02970@gmail.com>
2025-12-31 17:07:00 +0530

    made some changes

--- src/index.js
+++ src/index.js
@@ -24,21 +24,9 @@ app.use(express.json())
 app.use(cookieParser())
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
-// app.get('/', (req, res) => {
-//     res.send(`API is running`)
-// })
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-    app.get((req, res) => {
-        res.sendFile(path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html'));
-    });
-} else {
-    app.get('/', (req, res) => {
-        res.send(`API is running`)
-    })
-}
+app.get('/', (req, res) => {
+    res.send(`API is running`)
+})
 
 app.use('/api/product', productroute)
 app.use('/api/user', userroute)
